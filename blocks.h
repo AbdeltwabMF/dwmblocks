5e786d1f0d6b27747a5d6c430733fa989ed89788
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-10-30 18:32:13 +0200

    Fix pacman update interval

--- blocks.h
+++ blocks.h
@@ -7,7 +7,7 @@ recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"", "sb-record", 2, 16},
-    {"", "sb-newsfeed", 180, 15},
+    {"", "sb-newsfeed", 60, 15},
     {"", "sb-layout", 0, 14},
     {"", "sb-next-prayer", 29, 13},
     {"", "sb-mail", 2, 12},
@@ -15,11 +15,11 @@ static const Block blocks[] = {
     {"", "sb-memory", 2, 10},
     {"", "sb-home", 2, 9},
     {"", "sb-root", 2, 8},
-    {"", "sb-internet", 1, 7},
+    {"", "sb-internet", 2, 7},
     {"", "sb-volume", 0, 6},
     {"", "sb-brightness", 0, 5},
     {"", "sb-battery", 2, 4},
-    {"", "sb-pacups", 2, 3},
+    {"", "sb-pacman-updates", 0, 3},
     {"", "sb-date", 2, 2},
     {"", "sb-clock", 1, 1},
 };
